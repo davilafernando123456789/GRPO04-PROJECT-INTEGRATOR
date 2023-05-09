23cc67ae0f485ea9585fee43edf33317cac656dc
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2023-05-08 22:44:38 -0500

    Primer commit

---
+++ GRPO04-PROJECT-INTEGRATOR/src/main/java/com/tecsup/maven/proyectoIntegrador/GRPO04_PROJECT_INTEGRATOR/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.maven.proyectoIntegrador.GRPO04_PROJECT_INTEGRATOR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GRPO04-PROJECT-INTEGRATOR/src/test/java/com/tecsup/maven/proyectoIntegrador/GRPO04_PROJECT_INTEGRATOR/AppTest.java
@@ -0,0 +1,20 @@
+package com.tecsup.maven.proyectoIntegrador.GRPO04_PROJECT_INTEGRATOR;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
